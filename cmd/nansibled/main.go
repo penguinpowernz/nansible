da93e8ee9c11173cff716df66f6bb513b94d8a5e
Your Name <robert@penguinpower.co.nz>
2022-05-24 22:39:35 +1200

    specify the nats/redis URLs

--- cmd/nansibled/main.go
+++ cmd/nansibled/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"flag"
+	"os"
+
+	"github.com/albrow/zoom"
 	"github.com/gin-gonic/gin"
 	"github.com/nats-io/nats.go"
+	"github.com/penguinpowernz/nansible/pkg/nansibled"
 )
 
 func main() {
-	nc, err := nats.Connect(nats.DefaultURL)
+	var createKey, redisURL, natsURL string
+	flag.StringVar(&createKey, "create-key", "", "create a new key to access the API with")
+	flag.StringVar(&redisURL, "r", os.Getenv("REDIS_URL"), "the redis URL to use")
+	flag.StringVar(&natsURL, "n", os.Getenv("NATS_URL"), "the NATS URL to use")
+	flag.Parse()
+
+	if natsURL == "" {
+		natsURL = nats.DefaultURL
+	}
+
+	if redisURL == "" {
+		redisURL = "127.0.0.1:6379"
+	}
+
+	nc, err := nats.Connect(natsURL)
+	if err != nil {
+		panic(err)
+	}
+
 	pool := zoom.NewPoolWithOptions(zoom.DefaultPoolOptions.WithAddress(redisURL).WithDatabase(8))
 
 	svr := nansibled.NewServer(nc, pool)
